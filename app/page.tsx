acf6b9a7bb006bda639de5d26d7f81004bafa268
elliottsax <elliottsaxton@gmail.com>
2026-03-02 23:18:21 -0600

    fix: Remove unused Footer import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/home/Hero";
 import Mission from "@/components/home/Mission";
 import UpcomingEvents from "@/components/home/UpcomingEvents";
-import Footer from "@/components/layout/Footer";
 
 export default function Home() {
   return (
